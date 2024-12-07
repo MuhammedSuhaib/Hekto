755bb0f9ef504ce6c3491e501f8648087ffad1cf
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2024-12-07 22:47:18 +0500

    prettier-plugin-tailwindcss

--- components/Top.tsx
+++ components/Top.tsx
@@ -1,20 +1,8 @@
 import React from "react";
 function Top() {
   return (
-    <div className="w-[1440px] h-[48px] bg-black text-white ">
-      <div className="flex gap-[231px]  absolute top-[12px] left-[445px]">
-        {/* <div>
-          Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
-          <u>ShopNow</u>
-        </div> */}
-        <div className="w-[474px] h-[18px]  text-[14px] font-normal">
-  Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! 
-  <u>ShopNow</u>
-</div>
-
-        <div className="flex gap-[5px]">
-          English</div>
-      </div>
+    <div className="">
+    
     </div>
   );
 }
