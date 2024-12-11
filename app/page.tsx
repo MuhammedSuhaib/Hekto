132f1200324eb038e0c6953f47e562dd7f727e81
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2024-12-11 21:00:30 +0500

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Mi from "@/components/Mi";
+import Nvbr from "@/components/nvbr";
 export default function Home() {
   return (
     <div className="bg-white w-screen overflow-x-hidden ">
       <Header />
-
+<Nvbr/>
       <Footer />
 
       <Mi />
---
+++ components/nvbr.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import Image from "next/image";
+import { Lato } from "next/font/google";
+const lato = Lato({ subsets: ["latin"], weight: ["400", "700"] });
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+
+function Nvbr() {
+  return (
+    <div>
+      <nav className="flex flex-col sm:items-center p-4 sm:flex-col sm:gap-7  lg:flex-row lg:justify-center xl:ml-[28vh] xl:justify-normal ">
+        <h2 className="text-[34px] flex justify-between font-bold text-[#0D0E43]">Hekto 
+        <div className="md:hidden">
+          <Sheet>
+            <SheetTrigger>
+              <Menu className="m-5 rounded-md bg-white" />
+            </SheetTrigger>
+            <SheetContent className= {`${lato.className}  bg-[#E7E6EF]  text-black`}>
+              <ul className="space-y-3">
+              <li className="flex text-[#FB2E86]">
+            Home
+            <Image
+              src="/pinkdown.png"
+              alt="Dropdown Icon"
+              width={12}
+              height={12}
+              className="size-3"
+            />
+          </li>
+          <li>Pages</li>
+          <li>Products</li>
+          <li>Blog</li>
+          <li>Shop</li>
+          <li>Contact</li>
+              </ul>
+            </SheetContent>
+          </Sheet>
+        </div>  </h2>
+
+        <ul
+          className={`${lato.className} hidden items-center justify-center gap-[35px] text-[16px] font-normal text-[#0D0E43] md:flex 2xl:ml-[88px]`}
+        >
+          <li className="flex items-center justify-center text-[#FB2E86]">
+            Home
+            <Image
+              src="/pinkdown.png"
+              alt="Dropdown Icon"
+              width={12}
+              height={12}
+              className="size-3"
+            />
+          </li>
+          <li>Pages</li>
+          <li>Products</li>
+          <li>Blog</li>
+          <li>Shop</li>
+          <li>Contact</li>
+        </ul>
+
+
+        <div className="  flex items-center justify-between sm:justify-end border-[2px] border-[#E7E6EF] 2xl:ml-[110px]">
+          <input type="text" className={`${lato.className} px-4`} />
+          <div className="flex size-[40px] items-center justify-center bg-[#FB2E86]">
+            <Image
+              src="/uil_search.png"
+              alt="Search Icon"
+              width={20}
+              height={20}
+            />
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Nvbr;
