39ea7bb7a0970b4258875e0dffdff059b33c670f
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2025-05-04 19:22:09 +0500

    now can recieve emails from contact form

--- app/Test/page.tsx
+++ app/Test/page.tsx
@@ -1,50 +1,5 @@
 
 export default function Page() {
 
-  return <div>Testing page
-{/* <form
-              className={`mb-[35px] w-[200px] sm:w-[445px] ${lato.className}`}
-            >
-              <div className="flex flex-col gap-6 sm:flex-row">
-                <div>
-                  <input
-                    type="text"
-                    id="name"
-                    placeholder="Your Name*"
-                    className="mb-[35px] h-[50px] w-[200px] rounded-[5px] border p-2 sm:w-[255px]"
-                  />
-                </div>
-                <div>
-                  <input
-                    type="text"
-                    id="email"
-                    placeholder="Your E-mail*"
-                    className="mb-[35px] h-[50px] w-[200px] rounded-[5px] border p-2 sm:w-[255px]"
-                  />
-                </div>
-              </div>
-              <div>
-                <input
-                  type="text"
-                  id="subject"
-                  placeholder="Subject*"
-                  className="mb-[45px] h-[50px] w-[200px] rounded-[5px] border p-2 sm:w-[534px]"
-                />
-              </div>
-              <div>
-                <textarea
-                  id="message"
-                  placeholder="Type Your Message*"
-                  rows={5}
-                  className="h-[197px] w-[200px] rounded-[5px] border p-2 sm:w-[534px]"
-                ></textarea>
-              </div>
-
-              <button className="mx-auto h-[41px]  w-[157px] rounded-[2px] bg-[#FB2E86] font-medium text-white transition-transform duration-300 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-[#7E33E0] lg:mx-0 lg:w-[135px]">
-                Send Mail
-              </button>
-            </form> */}
-
-
-  </div>;
+  return <div>Testing page</div>;
 }
